0d47b53637d5adfa0df935a9b75e6226fe8eecb7
Daniel Kershaw <d.kershaw1@lancaster.ac.uk>
2016-04-03 21:08:28 +0100

    Adding file to generate in the input files.

--- tools/GraphPrep.py
+++ tools/GraphPrep.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 import community
 import networkx as nx
 import pandas as pd
@@ -7,27 +9,75 @@ class GraphPrep:
     def __init__(self, path):
         '''Constructor'''
         edges = pd.read_csv(path, header=0)
-        print(edges.columns.values)
-        if "weight" in edges.columns.values.tolist():
-            self.G = nx.from_pandas_dataframe(edges, 'source', 'target', ['weight'])
-        else:
-            self.G = nx.from_pandas_dataframe(edges, 'source', 'target')
+        self.l = edges.columns.values.tolist()
+        self.l.remove('source')
+        self.l.remove('target')
+        self.G = nx.from_pandas_dataframe(edges, 'source', 'target', self.l)
 
     def swap_edges(self):
         # Random small world
         # Random mix of edges
         # Reverso of edges
-        self.G = nx.double_edge_swap(self.G, 1, 100)
+        self.G = nx.double_edge_swap(self.G, 50, 150)
+        return self
+
+    def shuffle_target(self):
+        edges = self.G.edges(data=True)
+
+        for e in edges:
+            self.G.remove_edge(e[0], e[1])
+
+        target = [e[1] for e in edges]
+        shuffle(target)
+
+        for e1, e2 in zip(edges, target):
+            self.G.add_edge(e1[0], e2, e1[2])
+
+        edges = self.G.edges(data=True)
+
+        return self
+
+    def shuffle_source(self):
+        edges = self.G.edges(data=True)
+
+        for e in edges:
+            self.G.remove_edge(e[0], e[1])
+
+        target = [e[0] for e in edges]
+        shuffle(target)
+
+        for e1, e2 in zip(edges, target):
+            self.G.add_edge(e2, e1[1], e1[2])
+
         return self
 
     def detect_communities(self):
         partition = community.best_partition(self.G)
         for n in partition:
             nx.set_node_attributes(self.G, 'community', {n: partition[n]})
+
+        self.l.append("community")
         return self
 
     def save_to_pickle(self, path):
         nx.write_gpickle(self.G, path, protocol=2)
+        return self
+
+    def save_edges_to_csv(self, path):
+        fh = open(path, 'wb')
+        nx.write_edgelist(self.G, fh, delimiter=',', data=self.l)
+        return self
+
+    def save_nodes_to_csv(self, path):
+        fh = open(path, 'wb')
+        for n in self.G.nodes(data=True):
+            line = []
+            line.append(str(n[0]))
+            for x in self.l:
+                if x in n[1]:
+                    line.append(str(n[1][x]))
+            fh.write(",".join(line))
+            fh.write("\n")
         return self
 
     def deg_graph(self):
@@ -35,7 +85,8 @@ class GraphPrep:
 
 
 if __name__ == '__main__':
-    GraphPrep("/Users/danielkershaw/PycharmProjects/DiffusionSimulation/data/Twitter_GEO_network.csv"). \
-        swap_edges(). \
-        detect_communities(). \
-        save_to_pickle("/Users/danielkershaw/PycharmProjects/DiffusionSimulation/data/Twitter_GEO_network.pickle")
+    GraphPrep("/Users/kershad1/PycharmProjects/DiffusionSimulation/data/user_reddit_comment_network_2014_significant"). \
+        shuffle_source(). \
+        shuffle_target(). \
+        save_edges_to_csv("./../data/edges-reddit-shuffle.csv").save_nodes_to_csv("./../data/nodes-reddit-shuffle.csv")
+    # save_to_pickle("../data/Twitter_GEO_network.pickle")

41a55e5a4c60c1213010fa354d6f9c1810efd7ea
Daniel Kershaw <danjamker@gmail.com>
2016-04-30 22:41:48 +0100

    Adding iPython Notebook to visulise simulation output

--- run-twitter-geo.sh
+++ run-twitter-geo.sh
@@ -3,4 +3,4 @@ python ./tools/GenerateFileList.py http://scc-culture-mind.lancs.ac.uk:50070/use
 python ./tools/GenerateFileList.py http://scc-culture-mind.lancs.ac.uk:50070/user/kershad1/twitter/geo-split/test ./data/twitter-geo-split-test
 cat ./data/twitter-geo-split-train ./data/twitter-geo-split-test > ./data/twitter-geo-split
 tar -zcvf compressed-code.tar.gz .
-python MRJobNetworkX.py -r hadoop -v ./data/twitter-geo --network ./networks/twitter_geo_network.gpickle -o ./output/twitter-geo-innovation --no-output --conf-path ./etc/mrjob.conf
+python MRJobNetworkX.py -r hadoop -v ./data/twitter-geo-split --network ./networks/twitter_geo_network.gpickle -o ./output/twitter-geo-innovation --no-output --conf-path ./etc/mrjob.conf

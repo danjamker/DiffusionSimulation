f49d3502dc5b3c6a608262276b43239b61aac0da
Daniel Kershaw <danjamker@gmail.com>
2016-05-01 22:45:06 +0100

    Reverting to pandas new api

--- MRJobNetworkX-50.py
+++ MRJobNetworkX-50.py
@@ -79,6 +79,7 @@ class MRJobNetworkX(MRJob):
                         ['numberOfActivations'], verify_integrity=True)
 
                     ruy = result_user_100.iloc[-1:]
+                    print len(ruy.index)
                     ruy = ruy.reindex([len(result_user.index)])
                     ray = result_act_100.iloc[-1:]
                     ray = ray.reindex([len(result_act.index)])

57ee02c871ce39210f4ee2ebcd769406a2fec235
Daniel Kershaw <danjamker@gmail.com>
2016-05-06 00:04:09 +0200

    Change entropy and add avrage exposure

---
+++ MRJobNetworkX-Simulation-Data.py
@@ -0,0 +1,116 @@
+from __future__ import division
+
+try:
+    from BytesIO import BytesIO
+except ImportError:
+    from io import BytesIO
+
+try:
+    from urlparse import urlparse
+except ImportError:
+    from urllib.parse import urlparse
+
+import networkx as nx
+import pandas as pd
+from mrjob.job import MRJob
+from mrjob.protocol import JSONValueProtocol
+from mrjob.step import MRStep
+
+import cascade
+import metrics
+
+
+class MRJobNetworkX(MRJob):
+    OUTPUT_PROTOCOL = JSONValueProtocol
+
+    def configure_options(self):
+        super(MRJobNetworkX, self).configure_options()
+        self.add_file_option('--network')
+        self.add_passthrough_option('--avrage', type='int', default=0, help='...')
+
+    def runCascade(self, C):
+        cas = C
+        idx = []
+        values = []
+        met = metrics.metric(cas.getGraph())
+        while True:
+            try:
+                cas.next()
+                met.add(cas.getInfectedNode())
+                values.append(met.asMap())
+                idx.append(cas.getStep())
+            except StopIteration:
+                break
+        return idx, values
+
+    def mapper_init(self):
+        self.G = nx.read_gpickle(self.options.network)
+        self.tmp = {node: 0 for node in self.G.nodes()}
+        nx.set_node_attributes(self.G, 'activated', self.tmp)
+
+    def mapper(self, _, line):
+        nx.set_node_attributes(self.G, 'activated', self.tmp)
+
+        idx, values = self.runCascade(cascade.actualCascade(line, self.G))
+        df = pd.DataFrame(values, index=idx)
+        result_user = df.drop_duplicates(subset='numberActivatedUsers', keep='first').set_index(
+            ['numberActivatedUsers'], verify_integrity=True)
+        result_act = df.drop_duplicates(subset='numberOfActivations', keep='first').set_index(
+            ['numberOfActivations'], verify_integrity=True)
+
+        yield "apple", {"file": line, "name": line.split("/")[-1],
+                        "result_user": result_user.to_json(),
+                        "result_act": result_act.to_json(),
+                        "raw": df.groupby(df.index).first().to_json()}
+
+    def combiner(self, key, values):
+        r_u_l = None
+        r_a_l = None
+        for v in values:
+            if r_u_l is None:
+                r_a_l = pd.read_json(v["result_act"])
+                r_u_l = pd.read_json(v["result_user"])
+            else:
+                r_u_l = pd.concat((r_u_l, pd.read_json(v["result_user"])))
+                r_u_l = r_u_l.groupby(r_u_l.index).mean()
+
+                r_a_l = pd.concat((r_a_l, pd.read_json(v["result_act"])))
+                r_a_l = r_a_l.groupby(r_a_l.index).mean()
+
+        yield key, {"result_user": r_u_l.to_json(), "result_act": r_a_l.to_json()}
+
+    def reducer(self, key, values):
+        r_u_l = None
+        r_a_l = None
+        for v in values:
+            if r_u_l is None:
+                r_a_l = pd.read_json(v["result_act"])
+                r_u_l = pd.read_json(v["result_user"])
+            else:
+                r_u_l = pd.concat((r_u_l, pd.read_json(v["result_user"])))
+                r_u_l = r_u_l.groupby(r_u_l.index).mean()
+
+                r_a_l = pd.concat((r_a_l, pd.read_json(v["result_act"])))
+                r_a_l = r_a_l.groupby(r_a_l.index).mean()
+
+        yield key, {"result_user": r_u_l.to_json(), "result_act": r_a_l.to_json()}
+
+    def steps(self):
+        if self.options.avrage == 1:
+            return [
+                MRStep(mapper_init=self.mapper_init,
+                       mapper=self.mapper,
+                       combiner=self.combiner,
+                       reducer=self.reducer
+                       )
+            ]
+        else:
+            return [
+                MRStep(mapper_init=self.mapper_init,
+                       mapper=self.mapper
+                       )
+            ]
+
+
+if __name__ == '__main__':
+    MRJobNetworkX.run()
--- cascade.py
+++ cascade.py
@@ -272,3 +272,45 @@ class actualCascade(cascade):
             raise StopIteration()
         except KeyError:
             pass
+
+
+class actualCascadeDF(cascade):
+    def __init__(self, JSON, G):
+        # self.G = deepcopy(G)
+        self.G = G
+        self.f = file
+        self.cascase_id = 1
+        self.step = 1
+        self.d = {}
+        self.activated = ""
+        dtf = pd.read_json(JSON)
+        self.df = dtf.sort_index();
+        self.dfi = self.df.iterrows();
+        self.step_time = None
+
+        # self.name_to_id = dict((d["name"], n) for n, d in self.G.nodes_iter(data=True))
+        self.name_to_id = dict((n, n) for n, d in self.G.nodes_iter(data=True))
+
+    def next(self):
+        try:
+            activate = next(self.dfi)
+            self.activated_name = activate[1]['id']
+            self.activated = self.name_to_id[self.activated_name]
+            if self.G.has_node(self.name_to_id[self.activated]):
+                nx.set_node_attributes(self.G, 'activated',
+                                       {self.activated: self.G.node[self.activated]['activated'] + 1})
+            else:
+                self.activated = None
+
+            self.step += 1
+
+        except EOFError:
+            raise StopIteration()
+
+        except IndexError:
+            raise StopIteration()
+
+        except StopIteration:
+            raise StopIteration()
+        except KeyError:
+            pass
